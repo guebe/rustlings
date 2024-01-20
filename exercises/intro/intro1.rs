8bcc338c44af3b3d7bbe14f709c0038c059c73c6
gue <gue@debian.home>
2024-01-20 23:56:41 +0100

    day 1

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -13,7 +13,6 @@
 // Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     println!("Hello and");
@@ -25,17 +24,7 @@ fn main() {
     println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
     println!(r#"                               |___/      "#);
     println!();
-    println!("This exercise compiles successfully. The remaining exercises contain a compiler");
-    println!("or logic error. The central concept behind Rustlings is to fix these errors and");
+    println!("This exercises contain a compiler");
     println!("solve the exercises. Good luck!");
-    println!();
-    println!("The source for this exercise is in `exercises/intro/intro1.rs`. Have a look!");
-    println!(
-        "Going forward, the source of the exercises will always be in the success/failure output."
-    );
-    println!();
-    println!(
-        "If you want to use rust-analyzer, Rust's LSP implementation, make sure your editor is set"
-    );
     println!("up, and then run `rustlings lsp` before continuing.")
 }
